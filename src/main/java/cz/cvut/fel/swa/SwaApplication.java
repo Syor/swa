52103cefd36fe923cb37ea747e51ba96800f454a
petrs <petr.syro@gmail.com>
2023-07-18 10:44:30 +0200

    initial commit

---
+++ src/main/java/cz/cvut/fel/swa/SwaApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.swa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaApplication.class, args);
+	}
+
+}
