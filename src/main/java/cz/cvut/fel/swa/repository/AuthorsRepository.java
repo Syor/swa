2729e33b3a7f1ff26ea10ca36adcf8de4d0d8aa8
petrs <petr.syro@gmail.com>
2023-08-30 17:40:17 +0200

    created repositories

---
+++ src/main/java/cz/cvut/fel/swa/repository/AuthorsRepository.java
@@ -0,0 +1,7 @@
+package cz.cvut.fel.swa.repository;
+
+import cz.cvut.fel.swa.models.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorsRepository /*extends JpaRepository<Author, Integer> */{
+}
---
+++ src/main/java/cz/cvut/fel/swa/repository/BooksRepository.java
@@ -0,0 +1,9 @@
+package cz.cvut.fel.swa.repository;
+
+import cz.cvut.fel.swa.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface BooksRepository /*extends JpaRepository<Book, String> */{
+
+}
