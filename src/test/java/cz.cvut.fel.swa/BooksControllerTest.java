e7832e99de5dbc49778cb92a249c821218512c86
petrs <petr.syro@gmail.com>
2023-09-05 20:21:46 +0200

    přidány testy pro books

--- src/test/java/cz.cvut.fel.swa/BooksControllerTest.java
+++ src/test/java/cz.cvut.fel.swa/BooksControllerTest.java
@@ -3,10 +3,8 @@ package cz.cvut.fel.swa;
 
 import cz.cvut.fel.swa.controller.AuthorController;
 import cz.cvut.fel.swa.controller.BookController;
-import cz.cvut.fel.swa.models.AuthorsResponse;
-import cz.cvut.fel.swa.models.BooksResponse;
-import cz.cvut.fel.swa.models.NewAuthor;
-import cz.cvut.fel.swa.models.NewBook;
+import cz.cvut.fel.swa.models.*;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,8 +30,7 @@ public class BooksControllerTest {
     private Integer authorId;
 
     @BeforeEach
-    public void setup()
-    {
+    public void setup() {
         ResponseEntity<?> entity1 = authorController.putAuthor(new NewAuthor("Pepa", "Vopička", "12.5.1986"));
 
         Assertions.assertEquals(HttpStatus.OK, entity1.getStatusCode());
@@ -42,15 +39,14 @@ public class BooksControllerTest {
 
     @Test
     @Transactional
-    public void addBooksTest()
-    {
-        ResponseEntity<?> entity1 = bookController.putBook(new NewBook("Title", "isbn", "1.1.1999", "CZ" , "egnre", authorId));
+    public void addBooksTest() {
+        ResponseEntity<?> entity1 = bookController.putBook(new NewBook("Title", "isbn", "1.1.1999", "CZ", "egnre", authorId));
 
         Assertions.assertEquals(HttpStatus.CREATED, entity1.getStatusCode());
         Assertions.assertNotNull(entity1.getBody());
         Assertions.assertEquals(String.class, entity1.getBody().getClass());
 
-        ResponseEntity<?> entity2 = bookController.putBook(new NewBook("Title", "isbn2", "1.1.1999", "CZ" , "egnre", authorId));
+        ResponseEntity<?> entity2 = bookController.putBook(new NewBook("Title", "isbn2", "1.1.1999", "CZ", "egnre", authorId));
 
         Assertions.assertEquals(HttpStatus.CREATED, entity2.getStatusCode());
 
@@ -58,5 +54,73 @@ public class BooksControllerTest {
 
         Assertions.assertNotNull(entityResponse.getBody());
         Assertions.assertEquals(2, entityResponse.getBody().getData().size());
+    }
+
+    @Test
+    @Transactional
+    public void getBookTest() {
+        NewBook newBook = new NewBook("Title", "isbn", "1.1.1999", "CZ", "egnre", authorId);
+        ResponseEntity<?> entity1 = bookController.putBook(newBook);
+        Assertions.assertEquals(HttpStatus.CREATED, entity1.getStatusCode());
+        Assertions.assertNotNull(entity1.getBody());
+        String isbn = (String) entity1.getBody();
+
+        ResponseEntity<Book> bookResponse = bookController.getBook(isbn);
+        Assertions.assertEquals(HttpStatus.OK, bookResponse.getStatusCode());
+        Assertions.assertNotNull(bookResponse.getBody());
+
+        Book responseBook = bookResponse.getBody();
+
+        Assertions.assertEquals(newBook.getAuthor(), responseBook.getAuthor());
+        Assertions.assertEquals(newBook.getPublished(), responseBook.getPublished());
+        Assertions.assertEquals(newBook.getLanguage(), responseBook.getLanguage());
+        Assertions.assertEquals(newBook.getTitle(), responseBook.getTitle());
+        Assertions.assertEquals(newBook.getGenre(), responseBook.getGenre());
+    }
+
+    @Test
+    @Transactional
+    public void modifyBookTest() {
+        NewBook newBook = new NewBook("Title", "isbn", "1.1.1999", "CZ", "egnre", authorId);
+        ResponseEntity<?> entity1 = bookController.putBook(newBook);
+        Assertions.assertEquals(HttpStatus.CREATED, entity1.getStatusCode());
+        Assertions.assertNotNull(entity1.getBody());
+        String isbn = (String) entity1.getBody();
+
+        NewBook modifiedBook = new NewBook("newTitle", isbn, null, null, "scifi", null);
+        ResponseEntity<Void> modifiedResponse = bookController.postBook(isbn, modifiedBook);
+        Assertions.assertEquals(HttpStatus.OK, modifiedResponse.getStatusCode());
+
+
+        ResponseEntity<Book> bookResponse = bookController.getBook(isbn);
+        Assertions.assertEquals(HttpStatus.OK, bookResponse.getStatusCode());
+        Assertions.assertNotNull(bookResponse.getBody());
+
+        Book responseBook = bookResponse.getBody();
+
+        Assertions.assertEquals(newBook.getAuthor(), responseBook.getAuthor());
+        Assertions.assertEquals(newBook.getPublished(), responseBook.getPublished());
+        Assertions.assertEquals(newBook.getLanguage(), responseBook.getLanguage());
+        Assertions.assertEquals(modifiedBook.getTitle(), responseBook.getTitle());
+        Assertions.assertEquals(modifiedBook.getGenre(), responseBook.getGenre());
+    }
+
+    @Test
+    @Transactional
+    public void deleteBookTest() {
+        NewBook newBook = new NewBook("Title", "isbn", "1.1.1999", "CZ", "egnre", authorId);
+        ResponseEntity<?> entity1 = bookController.putBook(newBook);
+        Assertions.assertEquals(HttpStatus.CREATED, entity1.getStatusCode());
+        Assertions.assertNotNull(entity1.getBody());
+        String isbn = (String) entity1.getBody();
+
+        ResponseEntity<Void> entity2 = bookController.deleteBook(isbn);
+        Assertions.assertEquals(HttpStatus.OK, entity2.getStatusCode());
+
+        ResponseEntity<Book> responseEntity1 = bookController.getBook(isbn);
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, responseEntity1.getStatusCode());
+
+        ResponseEntity<Void> responseEntity2 = bookController.deleteBook(isbn);
+        Assertions.assertEquals(HttpStatus.NOT_FOUND, responseEntity2.getStatusCode());
     }
 }
